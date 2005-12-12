c335b13340a6bb1a7cc6f3d68133998e1e86b489
Jed Liu <liujed@cs.cornell.edu>
2005-12-12 21:30:41 +0000

    Insert objects into the core.

--- src/tests/HelloWorld.java
+++ src/tests/HelloWorld.java
@@ -10,20 +10,97 @@ public class HelloWorld {
 	      3372)));
     Client client = new Client(core, 30, 3);
 
-    // Attempt to read an (hopefully-invalid) OID just to see what happens.
+    // Assume we have a fresh core.
+
+    // Attempt to read OID 0; should get null.
     System.out.println();
-    System.out.println("Attempting to read OID 0");
+    System.out.println("Attempting to read OID 0; should get null");
     try {
       System.out.println("Got " + client.readOID(0L));
     } catch (AccessError e) {
       System.out.println("Access error: " + e.getMessage());
     }
 
-    // Attempt to write an (hopefully-invalid) OID just to see what happens.
+    // Attempt to write OID 0; should fail.
     System.out.println();
-    System.out.println("Attempting to write OID 0");
+    System.out.println("Attempting to write OID 0; should fail");
     try {
-      if (client.writeOID(0L, new DString("moo")))
+      if (client.writeOID(0L, new Public()))
+	System.out.println("Succeeded");
+      else
+	System.out.println("Failed");
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to insert at OID 0; should succeed.
+    System.out.println();
+    System.out.println("Attempting to insert at OID 0; should succeed");
+    try {
+      if (client.insertOID(0L, new Public()))
+	System.out.println("Succeeded");
+      else
+	System.out.println("Failed");
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to read OID 0; should succeed.
+    System.out.println();
+    System.out.println("Attempting to read OID 0; should succeed");
+    try {
+      System.out.println("Got " + client.readOID(0L));
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to write that OID; should succeed.
+    System.out.println();
+    System.out.println("Attempting to write OID 0; should succeed");
+    try {
+      if (client.writeOID(0L, new Writable()))
+	System.out.println("Succeeded");
+      else
+	System.out.println("Failed");
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to read that OID; should fail with access error.
+    System.out.println();
+    System.out.println("Attempting to read OID 0; should fail with access error");
+    try {
+      System.out.println("Got " + client.readOID(0L));
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to write that OID; should succeed.
+    System.out.println();
+    System.out.println("Attempting to write OID 0; should succeed");
+    try {
+      if (client.writeOID(0L, new Readable()))
+	System.out.println("Succeeded");
+      else
+	System.out.println("Failed");
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to read that OID; should succeed.
+    System.out.println();
+    System.out.println("Attempting to read OID 0; should succeed");
+    try {
+      System.out.println("Got " + client.readOID(0L));
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    // Attempt to write that OID; should fail with access error.
+    System.out.println();
+    System.out.println("Attempting to write OID 0; should fail with access error");
+    try {
+      if (client.writeOID(0L, new Public()))
 	System.out.println("Succeeded");
       else
 	System.out.println("Failed");
@@ -39,12 +116,19 @@ public class HelloWorld {
     for (int i = 0; i < oids.length; i++) System.out.println(oids[i]);
   }
 
-  static class DString extends DObject {
-    public String value;
+  static class Readable extends DObject {
+    public boolean canRead(Principal client) { return true; }
+    public boolean canWrite(Principal client) { return false; }
+  }
 
-    public DString(String s) {
-      this.value = s;
-    }
+  static class Writable extends DObject {
+    public boolean canRead(Principal client) { return false; }
+    public boolean canWrite(Principal client) { return true; }
+  }
+
+  static class Public extends DObject {
+    public boolean canRead(Principal client) { return true; }
+    public boolean canWrite(Principal client) { return true; }
   }
 }
 
