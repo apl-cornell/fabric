14dee63f658734cebf90f290e749801e5794462c
Jed Liu <liujed@cs.cornell.edu>
2005-12-12 19:24:21 +0000

    Checkpointing.

--- src/tests/HelloWorld.java
+++ src/tests/HelloWorld.java
@@ -9,9 +9,22 @@ public class HelloWorld {
       new Core(Collections.singletonList(new InetSocketAddress("localhost", 
 	      3372)));
     Client client = new Client(core, 30, 3);
+
+    // Attempt to read an (hopefully-invalid) OID just to see what happens.
+    System.out.println("Attempting to read OID 0");
+    try {
+      System.out.println("Got " + client.readOID(0L));
+    } catch (AccessError e) {
+      System.out.println("Access error: " + e.getMessage());
+    }
+
+    System.out.println();
+
+    // Get some OIDs.
+    System.out.println("Obtaining new OIDs...");
     long[] oids = client.getNewOIDs();
-    for (int i = 0; i < oids.length; i++)
-      System.out.println(oids[i]);
+    System.out.println("Got " + oids.length + " leases:");
+    for (int i = 0; i < oids.length; i++) System.out.println(oids[i]);
   }
 }
 
