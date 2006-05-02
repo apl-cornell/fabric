f963503c4b1a2c149318cac4f956f4e50c99e722
Jed Liu <liujed@cs.cornell.edu>
2006-05-02 22:44:36 +0000

    Encrypting and decrypting of DObjects.

--- src/tests/HelloWorld.java
+++ src/tests/HelloWorld.java
@@ -1,12 +1,15 @@
 import diaspora.common.*;
 import diaspora.client.*;
+import java.io.*;
 import java.security.*;
 import java.util.*;
+import javax.crypto.*;
 
 public class HelloWorld {
   public static void main(String[] args) throws Throwable {
     // Generate a key pair for signing stuff.
-    KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+    KeyPairGenerator keygen =
+      KeyPairGenerator.getInstance(Util.ALG_PUBLIC_KEY_GEN);
     KeyPair keyPair = keygen.genKeyPair();
 
     Client client = new Client(1000, 50, 2, 3);
@@ -26,7 +29,7 @@ public class HelloWorld {
     System.out.println();
     System.out.println("Attempting to write OID 0; should fail");
     try {
-      if (client.writeOID(0L, new TestObj(keyPair)))
+      if (client.writeOID(0L, makeTestObj(keyPair)))
 	System.out.println("Succeeded");
       else
 	System.out.println("Failed");
@@ -38,7 +41,7 @@ public class HelloWorld {
     System.out.println();
     System.out.println("Attempting to insert at OID 0; should succeed");
     try {
-      if (client.insertOID(0L, new TestObj(keyPair)))
+      if (client.insertOID(0L, makeTestObj(keyPair)))
 	System.out.println("Succeeded");
       else
 	System.out.println("Failed");
@@ -59,7 +62,7 @@ public class HelloWorld {
     System.out.println();
     System.out.println("Attempting to write OID 0; should succeed");
     try {
-      if (client.writeOID(0L, new TestObj(keyPair)))
+      if (client.writeOID(0L, makeTestObj(keyPair)))
 	System.out.println("Succeeded");
       else
 	System.out.println("Failed");
@@ -80,7 +83,7 @@ public class HelloWorld {
     System.out.println();
     System.out.println("Attempting to write OID 0; should succeed");
     try {
-      if (client.writeOID(0L, new TestObj(keyPair)))
+      if (client.writeOID(0L, makeTestObj(keyPair)))
 	System.out.println("Succeeded");
       else
 	System.out.println("Failed");
@@ -101,7 +104,7 @@ public class HelloWorld {
     System.out.println();
     System.out.println("Attempting to write OID 0; should fail with access error");
     try {
-      if (client.writeOID(0L, new TestObj(keyPair)))
+      if (client.writeOID(0L, makeTestObj(keyPair)))
 	System.out.println("Succeeded");
       else
 	System.out.println("Failed");
@@ -115,26 +118,34 @@ public class HelloWorld {
     long[] oids = client.getNewOIDs(0);
     System.out.println("Got " + oids.length + " leases:");
     for (int i = 0; i < oids.length; i++) System.out.println(oids[i]);
+
+    try {
+      SecretKey key = KeyGenerator.getInstance(Util.ALG_SECRET_KEY_GEN).generateKey();
+      System.out.println(makeTestObj(keyPair).encrypt(key));
+      System.out.println(makeTestObj(keyPair).encrypt(key).decrypt(key));
+    } catch (Exception e) { throw new RuntimeException(e); }
   }
 
-  static class TestObj extends DObject {
-    TestObj(KeyPair keys) {
-      super(0, 0, keys.getPublic(), new byte[0], 0, new byte[0]);
-      try {
-	Signature signature = Signature.getInstance(Util.ALG_SIGNATURE);
-	signature.initSign(keys.getPrivate());
-	signature.update(Util.toArray(this));
-	this.signature = signature.sign();
-      } catch (NoSuchAlgorithmException e) {
-	throw new RuntimeException(e);
-      } catch (InvalidKeyException e) {
-	throw new RuntimeException(e);
-      } catch (SignatureException e) {
-	throw new RuntimeException(e);
-      } catch (java.io.IOException e) {
-	throw new RuntimeException(e);
-      }
+  static DObject.Unencrypted makeTestObj(KeyPair keys) {
+    DObject.Unencrypted result = new DObject.Unencrypted(0, 0, keys.getPublic(),
+	null, new byte[0], 0, new byte[0], new Serializable() { });
+
+    try {
+      Signature signature = Signature.getInstance(Util.ALG_SIGNATURE);
+      signature.initSign(keys.getPrivate(), new SecureRandom());
+      signature.update(Util.toArray(result));
+      result.signature = signature.sign();
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    } catch (InvalidKeyException e) {
+      throw new RuntimeException(e);
+    } catch (SignatureException e) {
+      throw new RuntimeException(e);
+    } catch (java.io.IOException e) {
+      throw new RuntimeException(e);
     }
+
+    return result;
   }
 }
 
