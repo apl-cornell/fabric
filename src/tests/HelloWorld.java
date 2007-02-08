365f3043733dea6ee62d6fb50738453ecde86e45
Jed Liu <liujed@cs.cornell.edu>
2007-02-08 19:43:20 +0000

    SSL support.

--- src/tests/HelloWorld.java
+++ src/tests/HelloWorld.java
@@ -1,10 +1,22 @@
 import diaspora.common.*;
 import diaspora.client.*;
+import java.io.FileInputStream;
 import java.util.*;
+import java.security.KeyStore;
 
 public class HelloWorld {
   public static void main(String[] args) throws Throwable {
-    Client client = new Client(null, 1000, 50, 2, 3);
+    if (args.length != 2) {
+      System.out.println("Usage: " + HelloWorld.class.getName()
+          + " keystore passwd");
+      System.exit(1);
+    }
+
+    KeyStore keyStore = KeyStore.getInstance("JKS");
+    char[] passwd = args[1].toCharArray();
+    keyStore.load(new FileInputStream(args[0]), passwd);
+
+    Client client = new Client(keyStore, passwd, keyStore, 1000, 50, 2, 3);
     Random rand = new Random();
 
     // Assume we have a fresh core.
@@ -21,12 +33,13 @@ public class HelloWorld {
     // Attempt to write OID 0; should fail.
     int curInt = rand.nextInt();
     System.out.println();
-    System.out.println("Attempting to write " + curInt + " to OID 0; should fail");
+    System.out.println("Attempting to write " + curInt
+        + " to OID 0; should fail");
     try {
       if (client.writeOID(0L, 0L, new TestObj(curInt)))
-	System.out.println("Succeeded");
+        System.out.println("Succeeded");
       else
-	System.out.println("Failed");
+        System.out.println("Failed");
     } catch (AccessError e) {
       System.out.println("Access error: " + e.getMessage());
     }
@@ -34,12 +47,13 @@ public class HelloWorld {
     // Attempt to insert at OID 0; should succeed.
     curInt = rand.nextInt();
     System.out.println();
-    System.out.println("Attempting to insert " + curInt + " at OID 0; should succeed");
+    System.out.println("Attempting to insert " + curInt
+        + " at OID 0; should succeed");
     try {
       if (client.insertOID(0L, 0L, new TestObj(curInt)))
-	System.out.println("Succeeded");
+        System.out.println("Succeeded");
       else
-	System.out.println("Failed");
+        System.out.println("Failed");
     } catch (AccessError e) {
       System.out.println("Access error: " + e.getMessage());
     }
@@ -56,19 +70,21 @@ public class HelloWorld {
     // Attempt to write that OID; should succeed.
     curInt = rand.nextInt();
     System.out.println();
-    System.out.println("Attempting to write " + curInt + " to OID 0; should succeed");
+    System.out.println("Attempting to write " + curInt
+        + " to OID 0; should succeed");
     try {
       if (client.writeOID(0L, 0L, new TestObj(curInt)))
-	System.out.println("Succeeded");
+        System.out.println("Succeeded");
       else
-	System.out.println("Failed");
+        System.out.println("Failed");
     } catch (AccessError e) {
       System.out.println("Access error: " + e.getMessage());
     }
 
     // Attempt to read that OID; should fail with access error.
     System.out.println();
-    System.out.println("Attempting to read OID 0; should fail with access error");
+    System.out
+        .println("Attempting to read OID 0; should fail with access error");
     try {
       System.out.println("Got " + client.readOID(0L, 0L));
     } catch (AccessError e) {
@@ -80,9 +96,9 @@ public class HelloWorld {
     System.out.println("Attempting to write OID 0; should succeed");
     try {
       if (client.writeOID(0L, 0L, new TestObj()))
-	System.out.println("Succeeded");
+        System.out.println("Succeeded");
       else
-	System.out.println("Failed");
+        System.out.println("Failed");
     } catch (AccessError e) {
       System.out.println("Access error: " + e.getMessage());
     }
@@ -98,12 +114,13 @@ public class HelloWorld {
 
     // Attempt to write that OID; should fail with access error.
     System.out.println();
-    System.out.println("Attempting to write OID 0; should fail with access error");
+    System.out
+        .println("Attempting to write OID 0; should fail with access error");
     try {
       if (client.writeOID(0L, 0L, new TestObj()))
-	System.out.println("Succeeded");
+        System.out.println("Succeeded");
       else
-	System.out.println("Failed");
+        System.out.println("Failed");
     } catch (AccessError e) {
       System.out.println("Access error: " + e.getMessage());
     }
@@ -113,23 +130,23 @@ public class HelloWorld {
     System.out.println("Obtaining new object numbers...");
     long[] oNums = client.getNewONums(0);
     System.out.println("Got " + oNums.length + " leases:");
-    for (int i = 0; i < oNums.length; i++) System.out.println(oNums[i]);
+    for (int i = 0; i < oNums.length; i++)
+      System.out.println(oNums[i]);
   }
-  
+
   static class TestObj extends DObject {
     int val;
-    
+
     public TestObj() {
       this(new Random().nextInt());
     }
-    
+
     public TestObj(int val) {
       this.val = val;
     }
-    
+
     public String toString() {
       return new Long(val).toString();
     }
   }
 }
-
