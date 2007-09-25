996a73666174aeab5f1065a7c0a4450d27a38de9
Michael George <mdgeorge@cs.cornell.edu>
2007-09-25 16:28:16 +0000

    Replaced new Foo with new Foo.$Impl, added location to constructor call.

--- src/compiler/fabric/extension/NewExt_c.java
+++ src/compiler/fabric/extension/NewExt_c.java
@@ -1,5 +1,37 @@
 package fabric.extension;
 
-public class NewExt_c extends LocatedExt_c {
+import java.util.LinkedList;
+import java.util.List;
 
+import fabric.visit.ProxyRewriter;
+import polyglot.ast.Expr;
+import polyglot.ast.New;
+import polyglot.ast.NodeFactory;
+import polyglot.ast.TypeNode;
+import polyglot.util.Position;
+
+
+public class NewExt_c extends LocatedExt_c {
+  @SuppressWarnings("unchecked")
+  @Override
+  public New rewriteProxies(ProxyRewriter pr) {
+    New call = (New) node();
+    NodeFactory nf = pr.nodeFactory();
+
+    List<Expr> newargs = new LinkedList<Expr>(call.arguments());
+    Expr location = location();
+    if (location == null)
+      location =
+          nf.Call(call.position(), nf.Id(Position.compilerGenerated(),
+              "$getCore"));
+    newargs.add(0, location);
+
+
+    TypeNode implType = nf.TypeNodeFromQualifiedName(call.objectType().position(),
+                                                     call.objectType().name() + ".$Impl");
+    call = call.objectType(implType);
+    call = (New) call.arguments(newargs);
+    
+    return call;
+  }
 }
