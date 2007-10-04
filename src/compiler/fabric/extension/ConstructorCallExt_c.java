145f4c4ed3daa2c8ac69bf2cc91b2dba37e9305e
Jed Liu <liujed@cs.cornell.edu>
2007-10-04 23:20:56 +0000

    Only translate a constructor call if it's instantiating a Fabric type.

--- src/compiler/fabric/extension/ConstructorCallExt_c.java
+++ src/compiler/fabric/extension/ConstructorCallExt_c.java
@@ -7,6 +7,7 @@ import polyglot.ast.ConstructorCall;
 import polyglot.ast.Expr;
 import polyglot.ast.Node;
 import polyglot.ast.NodeFactory;
+import polyglot.types.Type;
 import polyglot.util.Position;
 import fabric.visit.ProxyRewriter;
 
@@ -15,9 +16,16 @@ public class ConstructorCallExt_c extends FabricExt_c {
   @Override
   public Node rewriteProxies(ProxyRewriter pr) {
     // Need to add a $location argument to super(...) and this(...) constructor
-    // calls if we are compiling a Diaspora class.
+    // calls if we are compiling a Fabric class.
     NodeFactory nf = pr.nodeFactory();
     ConstructorCall call = node();
+
+    // Ensure that we're translating a Fabric type.
+    Type containerType = call.constructorInstance().container();
+    if (!"java.lang.Object".equals(containerType.toString())
+        && !pr.typeSystem().isFabric(containerType))
+      return super.rewriteProxies(pr);
+
     List<Expr> args = new LinkedList<Expr>(call.arguments());
     args.add(0, nf.AmbExpr(Position.compilerGenerated(), nf.Id(Position
         .compilerGenerated(), "$location")));
