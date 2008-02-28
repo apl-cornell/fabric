6c0c7b93d59ba39a4a9e30b698e282b8905a829a
Michael George <mdgeorge@cs.cornell.edu>
2008-02-28 20:54:55 +0000

    Added a nice little convenience method for looping over multiple iterables.

--- src/system/fabric/common/Util.java
+++ src/system/fabric/common/Util.java
@@ -1,6 +1,8 @@
 package fabric.common;
 
 import java.io.*;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public final class Util {
   public static final String ALG_SIGNATURE = "MD5withRSA";
@@ -33,6 +35,66 @@ public final class Util {
     } catch( final IOException exc ) {
     	throw new InternalError( exc );
     }
+  }
+
+  /**
+   * Returns an iterable that iterates over the elements of the iterables passed
+   * in. The common use is:
+   * 
+   * <pre>
+   * for (T o : Utils.chain(a, b, c))
+   *  o.f()
+   * </pre>
+   * 
+   * This is intended to be more efficient and easier than creating a new
+   * collection containing the contents.
+   * 
+   * @param <T>
+   *          the types of the elements of the iterables
+   * @param iters
+   *          the iterables to chain
+   * @return an object that can be used to traverse iters in order
+   */
+  public static <T> Iterable<T> chain(final Iterable<T> ... iters) {
+    return new Iterable<T> () {
+      public Iterator<T> iterator() {
+        return new Iterator<T>() {
+          private int i = 0;
+          private Iterator<T> current = null;
+
+          /**
+           * Skip over any empty iters. After this method returns, either
+           * current is null (and there are no further elements to return), or
+           * current.hasNext().
+           */
+          private void advance() {
+            while (current == null && i < iters.length)
+            {
+              current = iters[i++].iterator();
+              if (!current.hasNext())
+                current = null;
+            }
+          }
+
+          public boolean hasNext() {
+            advance();
+            return current != null;
+          }
+
+          public T next() {
+            advance();
+            if (current == null)
+              throw new NoSuchElementException();
+            return current.next();
+          }
+
+          public void remove() {
+            // TODO Auto-generated method stub
+
+          }
+        };
+      }
+    };
   }
 }
 
