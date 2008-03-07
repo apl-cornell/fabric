6fc87318a62c7d56401175a0f021cf0f3ffed243
Michael George <mdgeorge@cs.cornell.edu>
2008-03-07 02:16:46 +0000

    Added a more readable log formatter for the console.  Put it in the client's configuration.  The core doesn't use it yet.

---
+++ src/system/fabric/common/ConsoleFormatter.java
@@ -0,0 +1,46 @@
+package fabric.common;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class ConsoleFormatter extends Formatter {
+
+  private static String lineSeparator = System.getProperty("line.separator");
+  private static long   start         = -1;
+  private static int    tagWidth      = 13;
+
+  @Override
+  public String format(LogRecord record) {
+    if (start < 0)
+      start = record.getMillis();
+    
+    StringBuilder result = new StringBuilder();
+    String level = record.getLevel().toString();
+    String time  = Long.toString(record.getMillis() - start);
+    
+    result.append("[");
+    result.append(level);
+    result.append(":");
+    for (int i = tagWidth - level.length() - time.length(); i > 0; i--)
+      result.append(" ");
+    result.append(time);
+    result.append("] ");
+    result.append(formatMessage(record));
+
+    result.append(lineSeparator);
+    if (record.getThrown() != null) {
+      try {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        record.getThrown().printStackTrace(pw);
+        pw.close();
+        result.append(sw.toString());
+      } catch (Exception ex) {
+      }
+    }
+
+    return result.toString();
+  }
+}
