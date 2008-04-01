d1a8212b42739d5c9c6b4e9ab89bfbaad66afb70
Michael George <mdgeorge@cs.cornell.edu>
2008-04-01 15:13:57 +0000

    Skeleton implementation for a client-side Core object that represents an in-process core and communicates directly with the core's transaction manager.

---
+++ src/system/fabric/client/InProcessCore.java
@@ -0,0 +1,73 @@
+package fabric.client;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import fabric.common.util.LongKeyMap;
+import fabric.core.PrepareRequest;
+import fabric.core.SerializedObject;
+import fabric.core.TransactionManager;
+import fabric.lang.Object;
+import fabric.lang.Object.$Impl;
+
+/**
+ * In-process implementation of the Core interface for use when a client is
+ * running in-process with a Core.  The operations work directly on the Core's
+ * TransactionManager object.
+ *
+ * @author mdgeorge
+ */
+public class InProcessCore extends RemoteCore {
+
+  InProcessCore(String name) {
+    super(name);
+    // TODO Auto-generated constructor stub
+  }
+
+  TransactionManager tm;
+  Principal          client;
+
+  @Override
+  public void abortTransaction(int transactionID) {
+    tm.abortTransaction(client, transactionID);
+  }
+
+  @Override
+  public void commitTransaction(int transactionID) throws TransactionCommitFailedException {
+    try {
+      tm.commitTransaction(client, transactionID);
+    } catch (TransactionCommitFailedException e) {
+      throw new fabric.client.TransactionCommitFailedException(e.getMessage());
+    }
+  }
+
+  @Override
+  public long createOnum() {
+    return tm.newOIDs(client, 1)[0];
+  }
+
+  @Override
+  public String name() {
+    return tm.getObjectStore().getName();
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public int prepareTransaction(Collection<$Impl> toCreate,
+      LongKeyMap<Integer> reads, Collection<$Impl> writes)
+      throws TransactionPrepareFailedException {
+    Collection<SerializedObject> serializedCreates = new ArrayList<SerializedObject> (toCreate.size());
+    Collection<SerializedObject> serializedWrites  = new ArrayList<SerializedObject> (writes.size());
+    
+    for ($Impl o : toCreate)
+      serializedCreates.add(new SerializedObject(o));
+    
+    for ($Impl o : writes)
+      serializedWrites.add(new SerializedObject(o));
+    
+    PrepareRequest req = new PrepareRequest(serializedCreates, serializedWrites, reads);
+    
+    return tm.prepare(client, req);
+  }
+}
