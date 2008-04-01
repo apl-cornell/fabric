68c6166d9b1f9bb079baa36b7f3e861d77202b7f
Michael George <mdgeorge@cs.cornell.edu>
2008-04-01 14:39:58 +0000

    Forgot to commit this earlier....modified resources to add writeFile

--- src/system/fabric/common/Resources.java
+++ src/system/fabric/common/Resources.java
@@ -5,8 +5,10 @@ package fabric.common;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Manages the loading of configuration files
@@ -18,6 +20,14 @@ public class Resources {
       filename += File.separator + name;
     
     return new FileInputStream(filename);
+  }
+  
+  public static OutputStream writeFile(String... relpath) throws IOException {
+    String filename = prefix;
+    for (String name : relpath)
+      filename += File.separator + name;
+    
+    return new FileOutputStream(filename);
   }
   
   private static final String prefix;
