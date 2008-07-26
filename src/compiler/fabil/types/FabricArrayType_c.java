b128759570c173ef498e084823185ff1a7035829
Jed Liu <liujed@cs.cornell.edu>
2008-07-26 21:26:25 +0000

    Array lengths no longer final.

--- src/compiler/fabil/types/FabricArrayType_c.java
+++ src/compiler/fabil/types/FabricArrayType_c.java
@@ -1,6 +1,7 @@
 package fabil.types;
 
 import polyglot.types.ArrayType_c;
+import polyglot.types.FieldInstance;
 import polyglot.types.Type;
 import polyglot.types.TypeSystem;
 import polyglot.util.Position;
@@ -14,6 +15,21 @@ public class FabricArrayType_c extends ArrayType_c {
     super(ts, pos, base);
   }
 
+  @SuppressWarnings("unchecked")
+  @Override
+  protected void init() {
+    boolean fixField = fields == null;
+
+    super.init();
+
+    if (fixField) {
+      // Make the length field non-final.
+      FieldInstance lengthField = lengthField();
+      lengthField = lengthField.flags(lengthField.flags().clearFinal());
+      fields.set(0, lengthField);
+    }
+  }
+
   /*
    * (non-Javadoc)
    * 
@@ -25,8 +41,8 @@ public class FabricArrayType_c extends ArrayType_c {
     FabricTypeSystem ts = (FabricTypeSystem) this.ts;
     if (ancestor.isCanonical() && !ancestor.isNull()
         && !ts.typeEquals(this, ancestor) && ancestor.isReference()
-        && ts.typeEquals(ancestor, ts.FObject())
-        && ts.isFabricArray(this)) return true;
+        && ts.typeEquals(ancestor, ts.FObject()) && ts.isFabricArray(this))
+      return true;
     return super.descendsFromImpl(ancestor);
   }
 
