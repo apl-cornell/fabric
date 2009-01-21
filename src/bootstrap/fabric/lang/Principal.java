7d8e296a947c19f921cf13111f1c7d899bd21458
Jed Liu <liujed@cs.cornell.edu>
2009-01-21 22:19:55 +0000

    fabric.lang.Principal.getInstance now takes in a Core for creating Principal objects.

--- src/bootstrap/fabric/lang/Principal.java
+++ src/bootstrap/fabric/lang/Principal.java
@@ -1,6 +1,33 @@
 package fabric.lang;
 
-public interface Principal extends jif.lang.Principal, fabric.lang.Object {
+import jif.lang.ActsForProof;
+import jif.lang.Closure;
+import jif.lang.Label;
+import fabric.client.Core;
 
+public interface Principal extends jif.lang.Principal, Object {
+  public static class $Proxy extends fabric.lang.Object.$Proxy implements
+      fabric.lang.Principal {
+
+    public $Proxy(Core core, long onum) {
+      super(core, onum);
+    }
+
+    public native boolean delegatesTo(jif.lang.Principal p);
+
+    public native boolean equals(jif.lang.Principal p);
+
+    public native ActsForProof findProofDownto(Core core, jif.lang.Principal q,
+        Object searchState);
+
+    public native ActsForProof findProofUpto(Core core, jif.lang.Principal p,
+        Object searchState);
+
+    public native boolean isAuthorized(Object authPrf, Closure closure,
+        Label lb, boolean executeNow);
+
+    public native String name();
+    
+    public static native Principal getInstance(Core core, String name);
+  }
 }
-
