1093cf809bb4adfc51ee60735f4fe6ae338bf1a8
Jed Liu <liujed@cs.cornell.edu>
2009-02-27 06:02:59 +0000

    Complain in stderr whenever we create an InternalError.

--- src/system/fabric/common/InternalError.java
+++ src/system/fabric/common/InternalError.java
@@ -1,9 +1,28 @@
 package fabric.common;
 
 public class InternalError extends Error {
-  public InternalError() {}
-  public InternalError(Throwable cause) { super(cause); }
-  public InternalError(String msg) { super(msg); }
-  public InternalError(String msg, Throwable cause) { super(msg, cause); }
-}
+  public InternalError() {
+  }
 
+  public InternalError(Throwable cause) {
+    super(cause);
+    reportCause(cause);
+  }
+
+  public InternalError(String msg) {
+    super(msg);
+  }
+
+  public InternalError(String msg, Throwable cause) {
+    super(msg, cause);
+    reportCause(cause);
+  }
+  
+  private void reportCause(Throwable cause) {
+    System.err.println("Creating InternalError exception:");
+    System.err.println("================ BEGIN STACK TRACE ================");
+    printStackTrace();
+    System.err.println("================= END STACK TRACE =================");
+    System.err.println();
+  }
+}
