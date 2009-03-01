44b0579a1b00bd60a22e67907ae4845e4ec31e9f
Jed Liu <liujed@cs.cornell.edu>
2009-03-01 23:53:06 +0000

    jif.lang.Principal now an abstract class instead of an interface.

--- src/bootstrap/jif/lang/PrincipalUtil.java
+++ src/bootstrap/jif/lang/PrincipalUtil.java
@@ -6,24 +6,30 @@ import fabric.lang.Object;
 
 public interface PrincipalUtil extends Object {
   public interface TopPrincipal extends Principal, Object {
-    public static class $Impl extends Object.$Impl implements TopPrincipal {
+    public static class $Impl extends Principal.$Impl implements TopPrincipal {
       public $Impl(Core core, Label label) throws UnreachableNodeException {
         super(core, label);
       }
 
+      @Override
       public native boolean delegatesTo(Principal p);
 
+      @Override
       public native boolean equals(Principal p);
 
+      @Override
       public native ActsForProof findProofDownto(Core core, Principal q,
           Object searchState);
 
+      @Override
       public native ActsForProof findProofUpto(Core core, Principal p,
           Object searchState);
 
+      @Override
       public native boolean isAuthorized(Object authPrf, Closure closure,
           Label lb, boolean executeNow);
 
+      @Override
       public native String name();
     }
   }
