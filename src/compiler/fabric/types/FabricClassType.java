d4d4b37a72ca8ac61ce67a4de6b639d9ffbee6df
Michael George <mdgeorge@cs.cornell.edu>
2009-07-20 17:47:46 +0000

    Added a comment warning that defaultFieldLabel can return null

--- src/compiler/fabric/types/FabricClassType.java
+++ src/compiler/fabric/types/FabricClassType.java
@@ -4,5 +4,10 @@ import jif.types.JifClassType;
 import jif.types.label.Label;
 
 public interface FabricClassType extends JifClassType {
+  
+  /**
+   * Return the label assiciated with this class' fields, or null if ths class
+   * has no fields.
+   */
   Label defaultFieldLabel();
 }
