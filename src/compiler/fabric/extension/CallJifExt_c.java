15bd1d7812fd353055dd87b6558c383c0458a18a
Xin Qi <qixin@cs.cornell.edu>
2009-08-02 05:20:19 +0000

    Substituting actuals for formals in label checking of method calls.

--- src/compiler/fabric/extension/CallJifExt_c.java
+++ src/compiler/fabric/extension/CallJifExt_c.java
@@ -1,13 +1,16 @@
 package fabric.extension;
 
-import java.util.List;
+import java.util.*;
 
 import fabric.ast.FabricCall;
+import fabric.types.FabricClassType;
 import fabric.types.FabricTypeSystem;
-import polyglot.ast.Node;
+import polyglot.ast.*;
+import polyglot.types.ReferenceType;
 import polyglot.types.SemanticException;
 import polyglot.types.Type;
 import polyglot.util.Position;
+import jif.ast.JifInstantiator;
 import jif.extension.JifCallExt;
 import jif.translate.ToJavaExt;
 import jif.types.*;
@@ -20,6 +23,36 @@ public class CallJifExt_c extends JifCallExt {
     super(toJava);
   }
   
+  protected void labelCheckArgs(LabelChecker lc, Call c, List<Label> actualLabels)
+  throws SemanticException
+  {
+      JifContext A = lc.context();
+      JifTypeSystem ts = lc.typeSystem();
+
+      // X_0 = X_null[n := A[pc]]
+      PathMap Xj = ts.pathMap();
+      Xj = Xj.N(A.pc());
+
+      for (Expr arg : (List<Expr>)c.arguments()) {
+          // A[pc := X_{j-1}[N]] |- Ej : Xj
+          A = (JifContext)A.pushBlock();
+          A.setPc(Xj.N(), lc);
+          arg = (Expr)lc.context(A).labelCheck(arg);
+          A = (JifContext)A.pop();
+
+          Xj = getPathMap(arg);
+          actualLabels.add(Xj.NV());
+      }
+
+//      // now that the actualArgs list is correct, we can constrain the args
+//      for (int i = 0; i < actualArgs.size(); i++) {
+//          Expr Ej = (Expr)actualArgs.get(i);
+//          constrainArg(lc, i, Ej, (Type)pi.formalTypes().get(i));
+//      }
+//
+//      return Xjoin;
+  }
+  
   @Override
   public Node labelCheck(LabelChecker lc) throws SemanticException {
     FabricCall c = (FabricCall)node();
@@ -30,8 +63,35 @@ public class CallJifExt_c extends JifCallExt {
       A = (JifContext)c.del().enterScope(A);
 
       JifMethodInstance mi = (JifMethodInstance)c.methodInstance();
+
+      Expr target = (Expr)c.target();
+      Label targetLabel = null;
+      
+      PathMap Xs = getPathMap(target);
+      if (target instanceof Special) {
+        targetLabel = ((FabricClassType)lc.context().currentClass()).thisLabel();
+      }
+      else {
+        targetLabel = Xs.NV();
+      }
+      
+      List<Type> formalTypes = (List<Type>)mi.formalTypes();
+      List<Label> formalLabels = new ArrayList<Label>(formalTypes.size());
+      for (Type ft : formalTypes) {
+        ArgLabel fl = (ArgLabel)ts.labelOfType(ft);
+        formalLabels.add(fl);
+      }
+      
+      List<Label> actualLabels = new ArrayList<Label>(c.arguments().size());
+      labelCheckArgs(lc, c, actualLabels);
+      
       Label entryLabel = mi.pcBound();
       Label returnLabel = mi.returnValueLabel();
+      
+      entryLabel = JifInstantiator.instantiate(entryLabel, A, target, (ReferenceType)target.type(), targetLabel, 
+          formalLabels, formalTypes, actualLabels, c.arguments(), Collections.EMPTY_LIST);
+      returnLabel = JifInstantiator.instantiate(returnLabel, A, target, (ReferenceType)target.type(), targetLabel, 
+          formalLabels, formalTypes, actualLabels, c.arguments(), Collections.EMPTY_LIST);
       
       Principal localPrincipal = ts.clientPrincipal(Position.compilerGenerated());
       Principal remotePrincipal = c.remoteClientPrincipal();
@@ -40,9 +100,12 @@ public class CallJifExt_c extends JifCallExt {
       // Note that we are calling the original method, rather than the _remote version
       ConfPolicy entryConfPolicy = ts.confProjection(entryLabel);
       IntegPolicy entryIntegPolicy = ts.integProjection(entryLabel);
+      
       for (Type ft : (List<Type>)mi.formalTypes()) {
         ArgLabel fl_ = (ArgLabel)ts.labelOfType(ft);
         Label fl = fl_.upperBound();
+        fl = JifInstantiator.instantiate(fl, A, target, (ReferenceType)target.type(), targetLabel, 
+            formalLabels, formalTypes, actualLabels, c.arguments(), Collections.EMPTY_LIST);
         
         ConfPolicy cp = ts.confProjection(fl);
         IntegPolicy ip = ts.integProjection(fl);
