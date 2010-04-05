ca970b84ce21f52d5fed358f0dbd7b258ad88626
Michael George <mdgeorge@cs.cornell.edu>
2010-04-05 20:29:22 +0000

    partial implementation of auth channels

--- src/system/fabric/common/net/handshake/HandshakeAuthenticated.java
+++ src/system/fabric/common/net/handshake/HandshakeAuthenticated.java
@@ -1,14 +1,37 @@
 package fabric.common.net.handshake;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.security.cert.Certificate;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 import fabric.common.net.NotImplementedException;
 import fabric.common.net.naming.SocketAddress;
 
 public class HandshakeAuthenticated implements HandshakeProtocol {
-
-  public ShakenSocket initiate(String name, SocketAddress addr) {
-    // TODO Auto-generated method stub
+  final SSLSocketFactory factory;
+  
+  public HandshakeAuthenticated() {
+    throw new NotImplementedException();
+  }
+  
+  public ShakenSocket initiate(String name, SocketAddress addr) throws IOException {
+    Socket s = new Socket(addr.getAddress(), addr.getPort());
+    s.setKeepAlive(false);
+    s.setTcpNoDelay(true);
+    
+    DataOutputStream out = new DataOutputStream(s.getOutputStream());
+    out.writeUTF(name);
+    
+    SSLSocket sock = (SSLSocket) factory.createSocket(s, name, addr.getPort(), true);
+    sock.setUseClientMode(true);
+    sock.startHandshake();
+    
+    Certificate[] certs = sock.getSession().getPeerCertificates();
+    
     throw new NotImplementedException();
   }
 
@@ -16,5 +39,4 @@ public class HandshakeAuthenticated implements HandshakeProtocol {
     // TODO Auto-generated method stub
     throw new NotImplementedException();
   }
-
 }
