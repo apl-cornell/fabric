ef6078f38c9aec6a57b101f872ae95e4dc924f6e
Michael George <mdgeorge@cs.cornell.edu>
2010-04-30 18:58:39 +0000

    make unit/net a package

--- tests/unit/net/BaselineServer.java
+++ tests/unit/net/BaselineServer.java
@@ -1,3 +1,5 @@
+package net;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
--- tests/unit/net/Test.java
+++ tests/unit/net/Test.java
@@ -1,3 +1,4 @@
+package net;
 
 public class Test {
   public static void main(String[] args) throws Exception {
