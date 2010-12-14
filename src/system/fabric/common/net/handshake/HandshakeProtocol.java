58c7cbaf121443df6e457d3eba48a024a8173d8b
Jed Liu <liujed@cs.cornell.edu>
2010-12-14 22:05:35 +0000

    comments

--- src/system/fabric/common/net/handshake/HandshakeProtocol.java
+++ src/system/fabric/common/net/handshake/HandshakeProtocol.java
@@ -6,6 +6,22 @@ import java.net.Socket;
 import fabric.common.net.naming.SocketAddress;
 
 public interface HandshakeProtocol {
+  /**
+   * Initiates a handshake with a remote host at the given address.
+   * 
+   * @param name
+   *          name of the virtual host
+   * @param addr
+   *          the IP address and port number for the host with which to initiate
+   *          a handshake.
+   */
   ShakenSocket initiate(String name, SocketAddress addr) throws IOException;
+  
+  /**
+   * Receives a handshake via the given socket.
+   * 
+   * @param s
+   *          the socket on which to receive a handshake.
+   */
   ShakenSocket receive(Socket s)                         throws IOException;
 }
