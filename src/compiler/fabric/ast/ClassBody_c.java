55e96b10675d809b2716da5d19fec902f5563e69
Owen Arden <owen@cs.cornell.edu>
2011-04-06 22:29:19 +0000

    missing commit

---
+++ src/compiler/fabric/ast/ClassBody_c.java
@@ -0,0 +1,30 @@
+package fabric.ast;
+
+import java.util.List;
+
+import fabric.extension.ClassBodyJifExt_c;
+
+import jif.ast.JifUtil;
+
+import polyglot.ast.ClassMember;
+import polyglot.ast.Node;
+import polyglot.util.Position;
+import polyglot.visit.NodeVisitor;
+
+public class ClassBody_c extends polyglot.ast.ClassBody_c {
+
+  @SuppressWarnings("unchecked")
+  public ClassBody_c(Position pos, List members) {
+    super(pos, members);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public Node visitChildren(NodeVisitor v) {
+    Node n = super.visitChildren(v);
+    ClassBodyJifExt_c ext = (ClassBodyJifExt_c) JifUtil.jifExt(n);
+    List<ClassMember> remote_wrappers = visitList(ext.remoteWrappers(),v);
+    ext.setRemoteWrappers(remote_wrappers);
+    return n;
+  }
+}
