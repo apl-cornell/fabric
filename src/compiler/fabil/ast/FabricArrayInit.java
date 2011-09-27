a945d9d23b400ef98330d2d511cac0fb93fd4622
Jed Liu <liujed@cs.cornell.edu>
2011-09-27 23:24:32 +0000

    Tweaks to make things build for me.

--- src/compiler/fabil/ast/FabricArrayInit.java
+++ src/compiler/fabil/ast/FabricArrayInit.java
@@ -5,7 +5,8 @@ import java.util.List;
 import polyglot.ast.Expr;
 
 public interface FabricArrayInit extends Annotated, polyglot.ast.ArrayInit {
-  FabricArrayInit elements(List<Expr> elements);
+  @SuppressWarnings("rawtypes")
+  FabricArrayInit elements(List elements);
   
   FabricArrayInit location(Expr location);
   FabricArrayInit label(Expr label);
