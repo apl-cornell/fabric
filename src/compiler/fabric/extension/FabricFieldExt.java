09f5818e9e1d56c637bcfe73614722111e126345
Krishnaprasad Vikram <kvikram@cs.cornell.edu>
2011-10-04 20:49:50 +0000

    Use the access label of the array elements as the access label of the length field.

--- src/compiler/fabric/extension/FabricFieldExt.java
+++ src/compiler/fabric/extension/FabricFieldExt.java
@@ -1,21 +1,24 @@
 package fabric.extension;
 
+import fabric.types.FabricArrayType;
+import fabric.types.FabricClassType;
+import fabric.types.FabricFieldInstance;
+import fabric.types.FabricTypeSystem;
+import polyglot.ast.Expr;
+import polyglot.ast.Field;
+import polyglot.ast.Node;
+import polyglot.ast.Receiver;
+import polyglot.types.ReferenceType;
+import polyglot.types.SemanticException;
 import jif.extension.JifFieldExt;
 import jif.translate.ToJavaExt;
 import jif.types.ConstraintMessage;
 import jif.types.JifContext;
 import jif.types.LabelConstraint;
+import jif.types.LabeledType;
 import jif.types.NamedLabel;
 import jif.types.label.Label;
 import jif.visit.LabelChecker;
-import polyglot.ast.Expr;
-import polyglot.ast.Field;
-import polyglot.ast.Node;
-import polyglot.ast.Receiver;
-import polyglot.types.SemanticException;
-import fabric.types.FabricClassType;
-import fabric.types.FabricFieldInstance;
-import fabric.types.FabricTypeSystem;
 
 
 public class FabricFieldExt extends JifFieldExt {
@@ -45,18 +48,47 @@ public class FabricFieldExt extends JifFieldExt {
       // Use the access label from the class rather than the field instance
       // This might change in future when we no longer require all field
       // access labels in a class to be the same
-      FabricClassType fct = (FabricClassType) ffi.container();
-      Label rhs = fct.getFoldedAccessLabel();
-      lc.constrain(new NamedLabel("pc ⊔ object label", lhs), LabelConstraint.LEQ,
-          new NamedLabel("access label", rhs), A.labelEnv(), n.position(),
-          new ConstraintMessage() {
-            
+      ReferenceType container = ffi.container();
+      if (container instanceof FabricArrayType) {
+        FabricArrayType fat = (FabricArrayType) container;
+        if (fts.isFabricArray(fat)) {
+          Label al = ffi.accessLabel();
+          if (al == null) {
+            // TODO Using the array label as its access label
+            // Need to add syntax to specify array access labels
+            al = ((LabeledType)fat.base()).labelPart();
+          }
+          lc.constrain(new NamedLabel("pc ⊔ object label", lhs), LabelConstraint.LEQ,
+              new NamedLabel("access label", al), A.labelEnv(), n.position(),
+              new ConstraintMessage() {
+
             public String msg() {
-              return "The join of the pc and the target label at a field access" +
-              		" should be bounded above by the field access label.";
+              return "The join of the pc and the target label at a array access" +
+              " should be bounded above by the array access label.";
             }
           }
-      );
+          );          
+        }
+      }
+      if (container instanceof FabricClassType) {
+        FabricClassType fct = (FabricClassType) container;
+        if (fts.isFabricClass(fct)) {
+          // We do not need access label checking for non fabric classes
+          // since their instances will always be read only locally
+          Label rhs = fct.getFoldedAccessLabel();
+          lc.constrain(new NamedLabel("pc ⊔ object label", lhs), LabelConstraint.LEQ,
+              new NamedLabel("access label", rhs), A.labelEnv(), n.position(),
+              new ConstraintMessage() {
+
+            @Override
+            public String msg() {
+              return "The join of the pc and the target label at a field access" +
+              " should be bounded above by the field access label.";
+            }
+          }
+          );
+        }
+      }
     }
     return super.labelCheck(lc);
   }
