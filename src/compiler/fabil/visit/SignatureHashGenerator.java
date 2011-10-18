1bcdd002c2759717eeb7a9b3ce9c93fe46315db5
Michael George <mdgeorge@cs.cornell.edu>
2011-10-18 21:06:14 +0000

    somehow these didn't get merged in...hopefully the rest of the merge was accurate

---
+++ src/compiler/fabil/visit/SignatureHashGenerator.java
@@ -0,0 +1,29 @@
+package fabil.visit;
+
+import fabil.types.FabILParsedClassType_c;
+import polyglot.ast.ClassDecl;
+import polyglot.ast.Node;
+import polyglot.types.ParsedClassType;
+import polyglot.visit.NodeVisitor;
+
+/**
+ * Ensures hashes in FabILParsedClassType_c instances are computed when
+ * compiling FabIL and Fabric signatures. This way, the hashes become part of
+ * the Polyglot serialized type information.
+ */
+public class SignatureHashGenerator extends NodeVisitor {
+
+  @Override
+  public Node leave(Node old, Node n, NodeVisitor v) {
+    if (n instanceof ClassDecl) {
+      ClassDecl decl = (ClassDecl) n;
+      ParsedClassType ct = decl.type();
+      if (ct instanceof FabILParsedClassType_c) {
+        ((FabILParsedClassType_c) ct).getClassHash();
+      }
+    }
+    
+    return super.leave(old, n, v);
+  }
+
+}
