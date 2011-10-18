42d6d3f443eaf72d2c46d57ece6b4821da6422e5
Jed Liu <liujed@cs.cornell.edu>
2011-10-18 19:15:06 +0000

    Reverting this file to r2502.  Auction example works again.  (This revert will be undone in a branch.)

--- src/compiler/fabric/types/FabricParsedClassType_c.java
+++ src/compiler/fabric/types/FabricParsedClassType_c.java
@@ -63,76 +63,61 @@ public class FabricParsedClassType_c extends JifParsedPolyType_c implements Fabr
   }
   
   @SuppressWarnings("unchecked")
-  /**
-   * This method returns the upper bound of the labels of 
-   * all the fields of this class and its superclasses.
-   * It computes this by taking a join of all labels concerned.
-   */
   public Label singleFieldLabel() {
     FabricTypeSystem ts = (FabricTypeSystem)typeSystem();
 
+    // TODO: check that the labels of fields in this class
+    // are the same as in the superType, if the superType
+    // defaultFieldLabel is not null.
     if (!fieldLabelFound) {
       if (ts.isFabricClass(this)) {
         FabricClassType superType = (FabricClassType)superType();
-
-        Label classLabel = ts.pairLabel(Position.compilerGenerated(), 
-            ts.bottomConfPolicy(Position.compilerGenerated()),
-            ts.topIntegPolicy(Position.compilerGenerated()));
-        
-        Label superLabel = superType == null ? classLabel : superType.singleFieldLabel();
-        
-        for (FieldInstance fi : (List<FieldInstance>)fields()) {
-          if (fi.flags().isStatic()) continue;
-          Type t = fi.type();
-          if (ts.isLabeled(t)) {
-            Label tslabel = ts.labelOfType(t);
-            classLabel = ts.tjoin(classLabel, tslabel);
+        if (superType != null && superType.singleFieldLabel() != null) {
+          singleFieldLabel = superType.singleFieldLabel();
+        }
+        else {
+          for (FieldInstance fi : (List<FieldInstance>)fields()) {
+            if (fi.flags().isStatic()) continue;
+            Type t = fi.type();
+            if (ts.isLabeled(t)) {
+              singleFieldLabel = ts.labelOfType(t);
+              break;
+            }
           }
         }
-        singleFieldLabel = superLabel == null ? classLabel : ts.tjoin(classLabel, superLabel);
       }
       fieldLabelFound = true;
     }
     return singleFieldLabel;
   }
   
-  /**
-   * This method returns the upper bound of the labels of 
-   * all the fields of this class and its superclasses.
-   * It computes this by taking a join of all labels concerned.
-   */
   public Label singleAccessLabel() {
     FabricTypeSystem ts = (FabricTypeSystem)typeSystem();
 
     if (!accessLabelFound) {
       if (ts.isFabricClass(this)) {
         FabricClassType superType = (FabricClassType)superType();
-        
-        Label classAccessLabel = ts.pairLabel(Position.compilerGenerated(), 
-            ts.bottomConfPolicy(Position.compilerGenerated()),
-            ts.topIntegPolicy(Position.compilerGenerated()));
-
-        Label superAccessLabel = superType == null ? classAccessLabel : superType.singleAccessLabel();
-
-        for (FieldInstance fi_ : (List<FieldInstance>)fields()) {
-          if (fi_.flags().isStatic()) continue;
-          FabricFieldInstance fi = (FabricFieldInstance) fi_;
-          Label al = fi.accessLabel();
-          if (al != null) {
-            // a tjoin is not necessary since access labels only have a conf component
-            classAccessLabel = ts.join(classAccessLabel, al);
+        if (superType != null && superType.singleAccessLabel() != null) {
+          singleAccessLabel = superType.singleAccessLabel();
+        }
+        else {
+          for (FieldInstance fi_ : (List<FieldInstance>)fields()) {
+            if (fi_.flags().isStatic()) continue;
+            FabricFieldInstance fi = (FabricFieldInstance) fi_;
+            Label al = fi.accessLabel();
+            if (al != null) {
+              singleAccessLabel = al;
+              break;
+            }
           }
         }
-        // a tjoin is not necessary since access labels only have a conf component
-        singleAccessLabel = superAccessLabel == null ? classAccessLabel : ts.join(classAccessLabel, superAccessLabel);
       }
       accessLabelFound = true;
     }
+
     return singleAccessLabel;
   }
   
-  // TODO The provider label should actually be folded into the access labels
-  // since each field of a class will be accessed from some method of the class, no?
   public Label getFoldedAccessLabel() {
     return singleAccessLabel();
     // XXX: this code folded in the provider label to the access label. This is
@@ -155,74 +140,60 @@ public class FabricParsedClassType_c extends JifParsedPolyType_c implements Fabr
     //    return singleAccessLabel;
   }
 
-  // XXX Why do we need these fabil'ed versions?
   public Label singleFabilAccessLabel() {
     FabricTypeSystem ts = (FabricTypeSystem)typeSystem();
-    if (isSubtype(ts.DelegatingPrincipal())) {
-      singleAccessLabel = null;
-      // recompute the access label
+
+    // or if this is a DelegatingPrincipal (XXX Principal instead?)
+    // then compute access label
+    if (!accessLabelFound || isSubtype(ts.DelegatingPrincipal())) {
+      FabricClassType superType = (FabricClassType)superType();
+      if (superType != null && superType.singleFabilAccessLabel() != null) {
+        singleAccessLabel = superType.singleFabilAccessLabel();
+      }
+      else {
+        for (FieldInstance fi : (List<FieldInstance>)fields()) {
+          if (fi.flags().isStatic()) continue;
+          Type t = fi.type();
+          if (ts.isLabeled(t)) {
+            singleAccessLabel = ts.labelOfType(t);
+            break;
+          }
+        }
+      }
+      accessLabelFound = true;
     }
-    return singleAccessLabel();
-//    FabricTypeSystem ts = (FabricTypeSystem)typeSystem();
-//
-//    // or if this is a DelegatingPrincipal (XXX Principal instead?)
-//    // then compute access label
-//    if (!accessLabelFound || isSubtype(ts.DelegatingPrincipal())) {
-//      FabricClassType superType = (FabricClassType)superType();
-//      if (superType != null && superType.singleFabilAccessLabel() != null) {
-//        singleAccessLabel = superType.singleFabilAccessLabel();
-//      }
-//      else {
-//        for (FieldInstance fi : (List<FieldInstance>)fields()) {
-//          if (fi.flags().isStatic()) continue;
-//          Type t = fi.type();
-//          if (ts.isLabeled(t)) {
-//            singleAccessLabel = ts.labelOfType(t);
-//            break;
-//          }
-//        }
-//      }
-//      accessLabelFound = true;
-//    }
-//    return singleAccessLabel;
+    return singleAccessLabel;
   }
   
   
   public Label singleFabilFieldLabel() {
+    // Type checking has been done, so all field labels are guaranteed to
+    // be the same
+    // ThisLabelChecker has already run and checked that 'this' is
+    // being used correctly.
     FabricTypeSystem ts = (FabricTypeSystem)typeSystem();
-    if (isSubtype(ts.DelegatingPrincipal())) {
-      singleFieldLabel = null;
-      // recompute the field label
+
+    // If the default field label has not been computed yet
+    // or if this is a DelegatingPrincipal (XXX Principal instead?)
+    // then compute defaultFieldLabel
+    if (!fieldLabelFound || isSubtype(ts.DelegatingPrincipal())) {
+      FabricClassType superType = (FabricClassType)superType();
+      if (superType != null && superType.singleFabilFieldLabel() != null) {
+        singleFieldLabel = superType.singleFabilFieldLabel();
+      }
+      else {
+        for (FieldInstance fi : (List<FieldInstance>)fields()) {
+          if (fi.flags().isStatic()) continue;
+          Type t = fi.type();
+          if (ts.isLabeled(t)) {
+            singleFieldLabel = ts.labelOfType(t);
+            break;
+          }
+        }
+      }
+      fieldLabelFound = true;
     }
-    return singleFieldLabel();
-    
-//    // Type checking has been done, so all field labels are guaranteed to
-//    // be the same
-//    // ThisLabelChecker has already run and checked that 'this' is
-//    // being used correctly.
-//    FabricTypeSystem ts = (FabricTypeSystem)typeSystem();
-//
-//    // If the default field label has not been computed yet
-//    // or if this is a DelegatingPrincipal (XXX Principal instead?)
-//    // then compute defaultFieldLabel
-//    if (!fieldLabelFound || isSubtype(ts.DelegatingPrincipal())) {
-//      FabricClassType superType = (FabricClassType)superType();
-//      if (superType != null && superType.singleFabilFieldLabel() != null) {
-//        singleFieldLabel = superType.singleFabilFieldLabel();
-//      }
-//      else {
-//        for (FieldInstance fi : (List<FieldInstance>)fields()) {
-//          if (fi.flags().isStatic()) continue;
-//          Type t = fi.type();
-//          if (ts.isLabeled(t)) {
-//            singleFieldLabel = ts.labelOfType(t);
-//            break;
-//          }
-//        }
-//      }
-//      fieldLabelFound = true;
-//    }
-//    return singleFieldLabel;
+    return singleFieldLabel;
   }
   
   public void removeMethod(MethodInstance mi) {
