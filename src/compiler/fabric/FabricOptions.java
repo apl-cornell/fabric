f8fbe483bbe2c0ee24236a9618133cada31aeb30
Jed Liu <liujed@cs.cornell.edu>
2011-10-31 22:08:21 +0000

    Reverted codebase-related changes up to and including r1866.

--- src/compiler/fabric/FabricOptions.java
+++ src/compiler/fabric/FabricOptions.java
@@ -31,10 +31,6 @@ public class FabricOptions extends JifOptions implements FabILOptions {
     delegate.bootclasspath = this.bootclasspath;
     return delegate.constructFabILClasspath();
   }
-  
-  public boolean dumpDependencies() {
-    return delegate.dumpDependencies;
-  }
 
   public int optLevel() {
     return delegate.optLevel();
