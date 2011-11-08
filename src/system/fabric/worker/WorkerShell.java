27d526740daa24e78c4ecab04e907b4c1a66a57e
Jed Liu <liujed@cs.cornell.edu>
2011-11-08 21:55:18 +0000

    Back-porting non-mobile changes in trunk through r2384.

--- src/system/fabric/worker/WorkerShell.java
+++ src/system/fabric/worker/WorkerShell.java
@@ -1,15 +1,11 @@
 package fabric.worker;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import jline.ConsoleReader;
 import jline.ConsoleReaderInputStream;
 import jline.Terminal;
-
 import fabric.common.exceptions.InternalError;
 
 public class WorkerShell {
@@ -25,8 +21,20 @@ public class WorkerShell {
     Terminal.setupTerminal();
   }
 
-  private static interface CommandHandler {
-    void handle(List<String> args) throws HandlerException;
+  private static abstract class CommandHandler {
+    private final String params;
+    private final String usage;
+
+    public CommandHandler(String usage) {
+      this(null, usage);
+    }
+
+    public CommandHandler(String params, String usage) {
+      this.params = params;
+      this.usage = usage;
+    }
+
+    public abstract void handle(List<String> args) throws HandlerException;
   }
 
   private static class HandlerException extends Exception {
@@ -39,9 +47,18 @@ public class WorkerShell {
     }
   }
 
+  /**
+   * Creates an interactive shell for the given worker that reads from System.in
+   * and outputs to System.out.
+   */
   public WorkerShell(Worker worker) throws IOException {
-    this(worker, new ConsoleReaderInputStream(new ConsoleReader(System.in,
-        new OutputStreamWriter(System.out))), System.out, true);
+    this(worker, new ConsoleReader(System.in,
+        new OutputStreamWriter(System.out)), System.out);
+  }
+
+  protected WorkerShell(Worker worker, ConsoleReader cr, PrintStream out) {
+    this(worker, new ConsoleReaderInputStream(cr), out, true);
+    cr.setDefaultPrompt("\n" + worker.config.name + "> ");
   }
 
   public WorkerShell(Worker worker, File in, PrintStream out)
@@ -70,30 +87,201 @@ public class WorkerShell {
     this.in.wordChars('/', '/');
 
     // Set up the command handlers.
-    this.handlers = new HashMap<String, CommandHandler>();
-    this.defaultHandler = new CommandHandler() {
-      public void handle(List<String> args) throws HandlerException {
-        if (args.size() == 0) {
-          throw new HandlerException("run: missing main class");
-        }
+    this.handlers = new TreeMap<String, CommandHandler>();
+    this.defaultHandler =
+        new CommandHandler("<APP> [ARG]...",
+            "Executes the Fabric application APP in this worker, passing ARGs "
+                + "as command-line arguments.") {
+          @Override
+          public void handle(List<String> args) throws HandlerException {
+            if (args.size() == 0) {
+              throw new HandlerException("run: missing main class");
+            }
 
-        // Run a Fabric program.
-        String mainClassName = args.get(0);
-        String[] appArgs = new String[args.size() - 1];
-        for (int i = 0; i < appArgs.length; i++) {
-          appArgs[i] = args.get(i + 1);
-        }
+            // Run a Fabric program.
+            String mainClassName = args.get(0);
+            String[] appArgs = new String[args.size() - 1];
+            for (int i = 0; i < appArgs.length; i++) {
+              appArgs[i] = args.get(i + 1);
+            }
 
-        try {
-          WorkerShell.this.worker.runFabricApp(mainClassName, appArgs);
-        } catch (Throwable e) {
-          throw new HandlerException("Exception encountered while running "
-              + mainClassName, e);
-        }
-      }
-    };
+            try {
+              WorkerShell.this.worker.runFabricApp(mainClassName, appArgs);
+            } catch (Throwable e) {
+              throw new HandlerException("Exception encountered while running "
+                  + mainClassName, e);
+            }
+          }
+        };
 
     this.handlers.put("run", defaultHandler);
+
+    this.handlers.put("fabc", new CommandHandler("Compiles a Fabric program.") {
+      @Override
+      public void handle(List<String> args) throws HandlerException {
+        throw new HandlerException("Not implemented yet!");
+      }
+    });
+
+    this.handlers.put("filc", new CommandHandler("Compiles a FabIL program.") {
+      @Override
+      public void handle(List<String> args) throws HandlerException {
+        throw new HandlerException("Not implemented yet!");
+      }
+    });
+
+    this.handlers.put("help", new CommandHandler("[CMD]", "Displays a help "
+        + "message for CMD.") {
+      final int SCREEN_WIDTH = 76;
+      final int INDENT = 2;
+
+      @Override
+      public void handle(List<String> args) {
+        String argument = null;
+        CommandHandler handler = null;
+        if (args.size() > 0) {
+          argument = args.iterator().next();
+          handler = WorkerShell.this.handlers.get(argument);
+        }
+
+        final PrintStream out = WorkerShell.this.out;
+        if (handler != null) {
+          // Print command usage template.
+          out.print("Usage: " + argument);
+          if (handler.params == null)
+            out.println();
+          else out.println(" " + handler.params);
+          out.println();
+
+          // Print command usage message.
+          printSpaces(out, INDENT);
+          int cur = INDENT;
+          StringTokenizer st = new StringTokenizer(handler.usage);
+          while (st.hasMoreTokens()) {
+            String s = st.nextToken();
+            if (cur + s.length() > SCREEN_WIDTH) {
+              out.println();
+              printSpaces(out, INDENT);
+              cur = INDENT;
+            }
+
+            out.print(s);
+            cur += s.length();
+            if (st.hasMoreTokens()) {
+              if (cur + 1 > SCREEN_WIDTH) {
+                out.println();
+                printSpaces(out, INDENT);
+                cur = INDENT;
+              } else {
+                out.print(" ");
+                cur++;
+              }
+            }
+          }
+
+          out.println();
+        } else {
+          // Show a list of commands.
+          if (argument != null) {
+            out.println("Unknown command: " + argument);
+            out.println();
+          }
+          showCommands();
+
+          out.println();
+          out.println("Use 'help [CMD]' to see help for a specific command.");
+        }
+      }
+
+      /**
+       * Displays a list of commands to the user.
+       */
+      protected void showCommands() {
+        final int PADDING = 2; // Amount of space between commands.
+        final int MAX_NUM_COLS =
+            (SCREEN_WIDTH + PADDING - INDENT) / (PADDING + 1);
+        Set<String> commandNames = WorkerShell.this.handlers.keySet();
+
+        int numRows = commandNames.size() / MAX_NUM_COLS;
+        if (commandNames.size() % MAX_NUM_COLS != 0) numRows++;
+
+        // Find a numRows that will fit.
+        int numCols = commandNames.size() / numRows;
+        if (commandNames.size() % numRows != 0) numCols++;
+        int[] colWidth = new int[numCols];
+        L: while (numRows != commandNames.size()) {
+          int width = INDENT - PADDING;
+          int curColWidth = 0;
+          int commandNum = 0;
+          for (String name : commandNames) {
+            if (name.length() > curColWidth) {
+              curColWidth = name.length();
+              if (width + curColWidth + PADDING > SCREEN_WIDTH) {
+                // Too wide.
+                numRows++;
+                numCols = commandNames.size() / numRows;
+                if (commandNames.size() % numRows != 0) numCols++;
+                colWidth = new int[numCols];
+                continue L;
+              }
+            }
+
+            commandNum++;
+            if (commandNum % numRows == 0) {
+              // New column.
+              width += curColWidth + PADDING;
+              colWidth[commandNum / numRows - 1] = curColWidth;
+              curColWidth = 0;
+            }
+          }
+          break;
+        }
+
+        // Format the output.
+        StringBuffer[] output = new StringBuffer[numRows];
+        for (int i = 0; i < numRows; i++) {
+          output[i] = new StringBuffer();
+          appendSpaces(output[i], INDENT);
+        }
+        int commandNum = 0;
+        int colNum = 0;
+        for (String name : commandNames) {
+          final int rowNum = commandNum % numRows;
+          output[rowNum].append(name);
+          int padding = 0;
+          if (name.length() < colWidth[colNum])
+            padding += colWidth[colNum] - name.length();
+          if (colNum * numRows < commandNames.size()) padding += PADDING;
+          appendSpaces(output[rowNum], padding);
+
+          commandNum++;
+          if (rowNum == numRows - 1) colNum++;
+        }
+
+        // Output.
+        WorkerShell.this.out.println("Available commands:");
+        for (StringBuffer line : output)
+          WorkerShell.this.out.println(line);
+      }
+
+      private void printSpaces(PrintStream out, int n) {
+        while (n-- > 0)
+          out.print(' ');
+      }
+
+      private void appendSpaces(StringBuffer sb, int n) {
+        while (n-- > 0)
+          sb.append(' ');
+      }
+    });
+
+    this.handlers.put("exit", new CommandHandler("Exits the worker shell.") {
+      @Override
+      public void handle(List<String> args) {
+        // Exiting is handled specially in the shell loop.
+        throw new InternalError();
+      }
+    });
   }
 
   public void run() {
@@ -144,22 +332,22 @@ public class WorkerShell {
    * 
    * @param command
    *          a list into which the command will be parsed.
-   * @return the parsed command, or null if there are no more comands.
+   * @return the parsed command, or null if there are no more commands.
    */
   private List<String> prompt(List<String> command) throws IOException {
-    out.println();
-    out.print(worker.config.name + "> ");
-    out.flush();
-
     command.clear();
 
     while (true) {
       int token = in.nextToken();
       switch (token) {
       case StreamTokenizer.TT_EOF:
-        if (command.isEmpty()) return null;
+        if (command.isEmpty()) {
+          if (interactive) out.println("exit");
+          return null;
+        }
         //$FALL-THROUGH$
       case StreamTokenizer.TT_EOL:
+      case ';':
         return command;
 
       case StreamTokenizer.TT_NUMBER:
