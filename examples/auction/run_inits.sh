9caa52f843d21e14af67e7f763a2667bde4a78bc
Jed Liu <liujed@cs.cornell.edu>
2011-11-13 01:48:38 +0000

    nuking extraneous scripts.

--- examples/auction/run_inits.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-screen -S brokerStore -X stuff "run auction.InitBrokerStore
-"
-screen -S airlineAStore -X stuff "run auction.InitAStore
-"
-screen -S airlineBStore -X stuff "run auction.InitBStore
-"
-screen -S userStore -X stuff "run auction.InitUserStore
-"
