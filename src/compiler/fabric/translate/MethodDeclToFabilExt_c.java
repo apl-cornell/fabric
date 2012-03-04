fc0d696edcb58705075440ffbab8186de3d4811b
Jed Liu <liujed@cs.cornell.edu>
2012-03-04 15:04:13 +0000

    bug fix

--- src/compiler/fabric/translate/MethodDeclToFabilExt_c.java
+++ src/compiler/fabric/translate/MethodDeclToFabilExt_c.java
@@ -1,95 +1,54 @@
 package fabric.translate;
 
-import java.util.Collections;
-import java.util.List;
-
 import jif.translate.JifToJavaRewriter;
 import jif.translate.MethodDeclToJavaExt_c;
-import jif.types.JifParsedPolyType;
-import jif.types.JifPolyType;
-import jif.types.JifTypeSystem;
 import polyglot.ast.Block;
-import polyglot.ast.Expr;
-import polyglot.ast.Formal;
 import polyglot.ast.If;
 import polyglot.ast.MethodDecl;
 import polyglot.ast.Node;
-import polyglot.ast.Stmt;
-import polyglot.ast.TypeNode;
-import polyglot.types.Flags;
 import polyglot.types.SemanticException;
 import polyglot.util.Position;
 import fabil.ast.FabILNodeFactory;
-import fabric.types.FabricParsedClassType;
 
 public class MethodDeclToFabilExt_c extends MethodDeclToJavaExt_c {
   @Override
   public Node toJava(JifToJavaRewriter rw) throws SemanticException {
-    MethodDecl md = (MethodDecl)super.toJava(rw);
-    
+    MethodDecl md = (MethodDecl) super.toJava(rw);
+
     if (md.body() == null) {
       // abstract method
       return md;
     }
-    
-    FabILNodeFactory nf = (FabILNodeFactory)rw.nodeFactory();
-    
+
+    FabILNodeFactory nf = (FabILNodeFactory) rw.nodeFactory();
+
     if (md.name().endsWith("_remote")) {
       // Fabric wrapper
       // Rewrite the else block to throw an exception
-      If ifStmt = (If)md.body().statements().get(0);
-      ifStmt = ifStmt.alternative(rw.qq().parseStmt("throw new fabric.worker.remote.RemoteCallLabelCheckFailedException();"));
+      If ifStmt = (If) md.body().statements().get(0);
+      ifStmt =
+          ifStmt
+              .alternative(rw
+                  .qq()
+                  .parseStmt(
+                      "throw new fabric.worker.remote.RemoteCallLabelCheckFailedException();"));
       return md.body(nf.Block(Position.compilerGenerated(), ifStmt));
     }
-    
+
     return md;
   }
-  
-  /** Rewrite static main(principal p, String[] args) {...} to
-   * static main(String[] args) {Principal p = Runtime.getUser(); {...} };
-   */
-  @SuppressWarnings("unchecked")
+
   @Override
-  public Node staticMainToJava(JifToJavaRewriter rw, MethodDecl n) throws SemanticException {
-      Formal formal0 = (Formal)n.formals().get(0); // the principal
-      Formal formal1 = (Formal)n.formals().get(1); // the string array
-      List<Formal> formalList = Collections.singletonList(formal1);
-
-      Block origBody = n.body();
-
-      JifTypeSystem jifTs = rw.jif_ts();
-      TypeNode type = rw.qq().parseType(jifTs.PrincipalClassName());
-      Expr init = rw.qq().parseExpr(jifTs.RuntimePackageName()
-          + ".Runtime.user(null)");
-
-      Stmt declPrincipal =
-          rw.java_nf().LocalDecl(origBody.position(),
-                             Flags.FINAL,
-                             type,
-                             rw.java_nf().Id(Position.compilerGenerated(), formal0.name()),
-                             init);
-    
-    // Translate the constraints and use them to guard the body.
-    Block newBody = guardWithConstraints(rw, origBody);
-    // Wrap with a transaction if there are constraints
-    FabricParsedClassType ct = (FabricParsedClassType) rw.currentClass();
-    if (!mi.constraints().isEmpty() || !ct.constraints().isEmpty())
-      newBody =
-          ((FabILNodeFactory) rw.java_nf()).Atomic(origBody.position(), newBody
-              .statements());
-      
-      newBody = rw.java_nf().Block(origBody.position(),
-                                         declPrincipal,
-                                         newBody);
-
-      n = rw.java_nf().MethodDecl(n.position(),
-                                  n.flags(),
-                                  n.returnType(),
-                                  rw.java_nf().Id(Position.compilerGenerated(), n.name()),
-                                  formalList,
-                                  n.throwTypes(),
-                                  newBody);
-      n = n.methodInstance(null);
-      return n;
+  protected Block guardWithConstraints(JifToJavaRewriter rw, Block b)
+      throws SemanticException {
+    boolean shouldGuard = true;
+    if (shouldGuard) {
+      @SuppressWarnings("unchecked")
+      Block newBody =
+          ((FabILNodeFactory) rw.java_nf()).Atomic(b.position(), super
+              .guardWithConstraints(rw, b).statements());
+      return newBody;
+    }
+    return b;
   }
 }
