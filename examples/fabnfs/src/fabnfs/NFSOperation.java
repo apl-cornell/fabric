d8bcef5e4077693ab79199ab12a2858dabcecbe9
Owen Arden <owen@cs.cornell.edu>
2012-07-04 17:13:26 +0000

    Update to more current transaction api.

--- examples/fabnfs/src/fabnfs/NFSOperation.java
+++ examples/fabnfs/src/fabnfs/NFSOperation.java
@@ -1,5 +1,8 @@
 package fabnfs;
 
+import fabric.worker.AbortException;
+import fabric.worker.Worker;
+
 class NFSOperation extends java.lang.Object implements Runnable, NFSConsts,
     RPCConsts {
   MTList input;
@@ -12,28 +15,24 @@ class NFSOperation extends java.lang.Object implements Runnable, NFSConsts,
 
   public void run() {
     for (;;) {
-      // System.out.print("thread waiting on nfs item queue\n");
-      NFSItem next = (NFSItem) input.Get();
-      // System.out.println("NFSOperation got item from queue xid=" +
-      // next.xid);
-
-      // wrapping this up in a transaction of its own
-      boolean commit = true;
-      fabric.worker.transaction.TransactionManager.getInstance()
-          .startTransaction();
+      final NFSItem next = (NFSItem) input.Get();
       try {
-        handler.Run(next.port, next.xid, next.procedure, next.packet);
-      } catch (final Throwable t) {
-        commit = false;
-        throw new fabric.worker.AbortException(t);
-      } finally {
-        if (commit) {
-          fabric.worker.transaction.TransactionManager.getInstance()
-              .commitTransaction();
-        } else {
-          fabric.worker.transaction.TransactionManager.getInstance()
-              .abortTransaction();
-        }
+        // System.out.print("thread waiting on nfs item queue\n");
+        // System.out.println("NFSOperation got item from queue xid=" +
+        // next.xid);
+  
+        // wrapping this up in a transaction of its own
+        boolean commit = true;
+        fabric.worker.transaction.TransactionManager.getInstance()
+            .startTransaction();
+        Worker.runInTopLevelTransaction(new Worker.Code<Void>() {
+          public Void run() {
+            handler.Run(next.port, next.xid, next.procedure, next.packet);
+            return null;
+          }  
+        }, false);
+      } catch (AbortException e) {
+         System.out.println("Aborted NFS item " + next);
       }
     }
   }
