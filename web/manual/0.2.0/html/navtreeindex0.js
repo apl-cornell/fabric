293841ddcbf8577879375b60a25771a90eb95498
Jed Liu <liujed@cs.cornell.edu>
2012-10-16 13:20:13 +0000

    Integrated 0.2.0 package and manual into website.

---
+++ web/manual/0.2.0/html/navtreeindex0.js
@@ -0,0 +1,17 @@
+var NAVTREEINDEX0 =
+{
+"citelist.html":[9],
+"compiling.html":[3],
+"credits.html":[8],
+"examples.html":[6],
+"index.html":[0],
+"index.html":[],
+"language.html":[2],
+"node-config.html":[4,0],
+"pages.html":[],
+"running.html":[5],
+"runtime.html":[4],
+"setup.html":[1],
+"start-nodes.html":[4,1],
+"versions.html":[7]
+};
