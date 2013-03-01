e13cd1e58ee2e9c9c54a1d4aea384830f7908e58
Tom Magrino <tmagrino@cs.cornell.edu>
2013-03-01 19:28:24 +0000

    Forgot to add CallInstance replacement.

---
+++ src/system/fabric/worker/memoize/CallInstance.java
@@ -0,0 +1,62 @@
+package fabric.worker.memoize;
+
+import java.util.Arrays;
+
+/**
+ * Represents a unique memoizable call in the Fabric system.
+ */
+public class CallInstance {
+
+  protected final fabric.lang.Object target;
+  protected final String method;
+  protected final Object[] arguments;
+
+  public CallInstance(fabric.lang.Object target, String method, Object...
+      arguments) {
+    this.target = target;
+    this.method = method;
+    this.arguments = arguments;
+  }
+
+  @Override
+  public String toString() {
+    String result = target.toString() + "." + method + "(";
+    boolean first = true;
+    for (Object arg : arguments) {
+      if (!first) {
+        result += ", ";
+      }
+      first = false;
+      result += arg.toString();
+    }
+    result += ")";
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o instanceof CallInstance) {
+      CallInstance oCallTup = (CallInstance) o;
+      return (target == oCallTup.target)
+        && (method == oCallTup.method)
+        && (Arrays.equals(arguments, oCallTup.arguments));
+    }
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return target.hashCode() ^ method.hashCode() ^ Arrays.hashCode(arguments);
+  }
+
+  /**
+   * ID number to uniquely represent this call signature.  This is
+   * deterministically computed based on the callee, methodName, and arguments.
+   * This should be usable wherever one could use a object ID while not
+   * conflicting with any OIDs in the system.
+   */
+  public long id() {
+    /* TODO: Actually implement */
+    return 0l;
+  }
+}
