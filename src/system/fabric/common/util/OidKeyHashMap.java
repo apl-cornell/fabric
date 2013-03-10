3f9d8c521945d33c78c9aaffcd07d8a4d8991d60
Jed Liu <liujed@cs.cornell.edu>
2013-03-10 22:22:22 +0000

    Bug fix.

--- src/system/fabric/common/util/OidKeyHashMap.java
+++ src/system/fabric/common/util/OidKeyHashMap.java
@@ -121,7 +121,7 @@ public final class OidKeyHashMap<V> implements Iterable<LongKeyMap<V>> {
   }
 
   public int size() {
-    int result = 0;
+    int result = hasNullEntry ? 1 : 0;
 
     for (LongKeyMap<V> submap : this)
       result += submap.size();
