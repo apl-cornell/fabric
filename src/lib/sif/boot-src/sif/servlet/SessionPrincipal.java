c5df8f356e5b02fc9b0ea7f5d2507bcd06230cd5
Owen Arden <owen@cs.cornell.edu>
2013-08-14 20:13:28 +0000

    Bootstrap source for SIF.

---
+++ src/lib/sif/boot-src/sif/servlet/SessionPrincipal.java
@@ -0,0 +1,9 @@
+package sif.servlet;
+
+import fabric.lang.security.*;
+
+/**
+ * A session principal. This principal represents an HTTP session.
+ */
+public interface SessionPrincipal extends AbstractPrincipal {
+}
