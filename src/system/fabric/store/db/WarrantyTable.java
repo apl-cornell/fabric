abb3686100a23550a374a18ccad0f71999c925da
Jed Liu <liujed@cs.cornell.edu>
2014-01-24 03:17:05 +0000

    Was throwing the wrong kind of InternalError.

--- src/system/fabric/store/db/WarrantyTable.java
+++ src/system/fabric/store/db/WarrantyTable.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 
 import fabric.common.Logging;
 import fabric.common.Warranty;
+import fabric.common.exceptions.InternalError;
 import fabric.common.util.Pair;
 
 /**
