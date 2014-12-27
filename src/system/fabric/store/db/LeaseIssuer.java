7c77a8fbb2bc0601216ad01bb4f7cf450bc51b03
Tom Magrino <tmagrino@cs.cornell.edu>
2014-12-26 16:10:24 -0800

    Avoid concurrent modification exception.

--- src/system/fabric/store/db/LeaseIssuer.java
+++ src/system/fabric/store/db/LeaseIssuer.java
@@ -314,7 +314,7 @@ public class LeaseIssuer<K, V extends Lease> {
         try {
           // Every second, wipe away stale entries.
           Thread.sleep(1000);
-          for (Long timeSlice : reverseTable.keySet()) {
+          for (Long timeSlice : new HashSet<>(reverseTable.keySet())) {
             if (((timeSlice.longValue() + 1) << 3) < System.currentTimeMillis()) {
               Set<Entry> toWipe = reverseTable.removeAll(timeSlice);
               for (Entry candidate : toWipe) {
