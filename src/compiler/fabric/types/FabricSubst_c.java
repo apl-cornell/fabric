eff2711df2f7c6ca588c842c8e5668360d7ab01e
Tom Magrino <tmagrino@cs.cornell.edu>
2015-11-07 23:22:59 -0500

    Correctly substitute access policies on fields.

--- src/compiler/fabric/types/FabricSubst_c.java
+++ src/compiler/fabric/types/FabricSubst_c.java
@@ -8,6 +8,7 @@ import jif.types.JifTypeSystem;
 import jif.types.Param;
 import jif.types.ParamInstance;
 import polyglot.types.ClassType;
+import polyglot.types.FieldInstance;
 
 public class FabricSubst_c extends JifSubst_c {
   public FabricSubst_c(JifTypeSystem ts,
@@ -24,5 +25,18 @@ public class FabricSubst_c extends JifSubst_c {
 
     return new FabricSubstClassType_c((JifTypeSystem) ts, t.position(),
         (JifClassType) t, this);
+  }
+
+  @Override
+  public <FI extends FieldInstance> FI substField(FI fi) {
+    fi = super.substField(fi);
+    if (fi instanceof FabricFieldInstance) {
+      FabricFieldInstance ffi = (FabricFieldInstance) fi;
+      if (ffi.accessPolicy() != null) {
+        FabricTypeSystem fts = (FabricTypeSystem) ts;
+        ffi.setAccessPolicy(substLabel(fts.toLabel(ffi.accessPolicy())).confProjection());
+      }
+    }
+    return fi;
   }
 }
