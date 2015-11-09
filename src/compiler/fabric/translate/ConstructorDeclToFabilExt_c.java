aaa4fde68444750737d2706f999043f2eb14c554
Tom Magrino <tmagrino@cs.cornell.edu>
2015-11-08 20:28:42 -0500

    Bypass begin/end access labels in constructor translation.

--- src/compiler/fabric/translate/ConstructorDeclToFabilExt_c.java
+++ src/compiler/fabric/translate/ConstructorDeclToFabilExt_c.java
@@ -3,15 +3,29 @@ package fabric.translate;
 import java.util.ArrayList;
 import java.util.List;
 
+import fabil.ast.FabILNodeFactory;
+
+import fabric.ast.FabricConstructorDecl;
+
 import jif.translate.ConstructorDeclToJavaExt_c;
 import jif.translate.JifToJavaRewriter;
+
 import polyglot.ast.MethodDecl;
 import polyglot.ast.Node;
 import polyglot.ast.Stmt;
 import polyglot.types.SemanticException;
-import fabil.ast.FabILNodeFactory;
+import polyglot.visit.NodeVisitor;
 
 public class ConstructorDeclToFabilExt_c extends ConstructorDeclToJavaExt_c {
+  @Override
+  public NodeVisitor toJavaEnter(JifToJavaRewriter rw)
+      throws SemanticException {
+    FabricConstructorDecl n = (FabricConstructorDecl) node();
+    // Bypass startLabel, returnLabel and constraints.
+    return ((JifToJavaRewriter) super.toJavaEnter(rw))
+        .bypass(n.beginAccessLabel()).bypass(n.endAccessLabel());
+  }
+
   @Override
   public Node toJava(JifToJavaRewriter rw) throws SemanticException {
     Node n = super.toJava(rw);
