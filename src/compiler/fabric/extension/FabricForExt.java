a0df250fbdc90061af3019bd7f29398c9dcdfd0c
Tom Magrino <tmagrino@cs.cornell.edu>
2016-05-18 22:37:27 -0400

    For loops are now checked in a way similar to while loops.

--- src/compiler/fabric/extension/FabricForExt.java
+++ src/compiler/fabric/extension/FabricForExt.java
@@ -1,9 +1,25 @@
 package fabric.extension;
 
+import java.util.List;
+
+import fabric.types.FabricContext;
+import fabric.types.FabricPathMap;
+import fabric.types.FabricTypeSystem;
+
 import jif.extension.JifForExt;
 import jif.translate.ToJavaExt;
+import jif.types.ConstraintMessage;
+import jif.types.JifContext;
+import jif.types.LabelConstraint;
+import jif.types.NamedLabel;
+import jif.types.PathMap;
+import jif.types.label.Label;
 import jif.visit.LabelChecker;
 
+import polyglot.ast.Branch;
+import polyglot.ast.For;
+import polyglot.ast.ForInit;
+import polyglot.ast.ForUpdate;
 import polyglot.ast.Node;
 import polyglot.types.SemanticException;
 import polyglot.util.SerialVersionUID;
@@ -16,9 +32,81 @@ public class FabricForExt extends JifForExt {
   }
 
   @Override
-  public Node labelCheckStmt(LabelChecker lc) throws SemanticException {
-    // TODO Auto-generated method stub
-    // TODO: Need to handle loop rules for CL
-    return super.labelCheckStmt(lc);
+  public Node checkLoop(LabelChecker lc, JifContext A, For fs,
+      List<ForInit> inits, PathMap Xinit) throws SemanticException {
+
+    FabricTypeSystem ts = (FabricTypeSystem) lc.typeSystem();
+    FabricContext Af = (FabricContext) A;
+
+    Label noAccesses = ts.noAccesses();
+
+    Label L1 = ts.freshLabelVariable(fs.position(), "for",
+            "conflict label for the for loop at " + fs.position());
+    Label L2 = ts.freshLabelVariable(fs.position(), "for",
+            "conflict label for end of the for loop at " +
+            fs.position());
+    Label loopEntryCL = Af.conflictLabel();
+
+    Af = (FabricContext) Af.pushBlock();
+
+    Af.setConflictLabel(L1);
+    Af.gotoConflictLabel(Branch.CONTINUE, null, L1);
+    Af.gotoConflictLabel(Branch.BREAK, null, L2);
+
+    Af = (FabricContext) Af.pushBlock();
+
+    fs = (For) super.checkLoop(lc, Af, fs, inits, Xinit);
+
+    Af = (FabricContext) Af.pop();
+    Af = (FabricContext) Af.pop();
+
+    FabricPathMap Xbody = (FabricPathMap) getPathMap(fs.body());
+    for (ForUpdate update : fs.iters()) {
+      FabricPathMap Xs = (FabricPathMap) getPathMap(update);
+      Xbody = Xbody.join(Xs);
+    }
+
+    if (!Xbody.CL().equals(ts.noAccesses()) ||
+        !loopEntryCL.equals(ts.noAccesses())) {
+      NamedLabel endCL;
+      if (Xbody.CL().equals(ts.noAccesses())) {
+        endCL = new NamedLabel("loop_entry_conflict_pc",
+            "label of the program counter just before the loop is executed",
+            loopEntryCL);
+      } else if (loopEntryCL.equals(ts.noAccesses())) {
+        endCL = new NamedLabel("for_body.CL",
+            "conflict label of normal termination of the loop body", Xbody.CL());
+      } else {
+        endCL = new NamedLabel("for_body.CL",
+            "conflict label of normal termination of the loop body", Xbody.CL())
+          .meet(lc, "loop_entry_conflict_pc",
+              "label of the program counter just before the loop is executed",
+              loopEntryCL);
+      }
+      // If this loop makes accesses, then we need to ensure that the starting
+      // stage is the same as the ending stage before another iteration.
+      lc.constrain(
+          new NamedLabel("loop_conflict_pc",
+            "conflict label at the top of the loop", L1),
+          LabelConstraint.LEQ,
+          endCL,
+          Af.labelEnv(), fs.position(), false, new ConstraintMessage() {
+            @Override
+            public String msg() {
+              return "The stage at the end of the for loop must be compatible " +
+                     "with the stage for evaluating the loop condition.";
+            }
+          });
+    }
+
+    FabricPathMap X = (FabricPathMap) getPathMap(fs);
+    X = X.setCL(ts.gotoPath(Branch.BREAK, null), noAccesses);
+    X = X.setCL(ts.gotoPath(Branch.CONTINUE, null), noAccesses);
+    X = X.CL(L2);
+
+    // update the conflict pc
+    Af.setConflictLabel(X.CL());
+
+    return updatePathMap(fs, X);
   }
 }
