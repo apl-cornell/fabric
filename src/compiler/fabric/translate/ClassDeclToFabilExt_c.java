apl-cornell/fabric

commit 5901506909366a5614e82e93c41597d025c4bfb1

Initialize object fragments in $initLabels(), not jif$init(). This ensures that the object's final fields are assigned first. Initializing the object fragments involves initializing their labels, which might depend on the final fields.
Travel example works again.

src/compiler/fabric/translate/ClassDeclToFabilExt_c.java
@@ -189,11 +189,13 @@ public class ClassDeclToFabilExt_c extends ClassDeclToJavaExt_c {
     FabricToFabilRewriter fabrw = (FabricToFabilRewriter) rw;
     List<Stmt> inits = new ArrayList<>();
 
-    // First, initialize the object fragments.
+    // First, create the object fragments.
+    // Don't initialize them yet, however. This will be done in the root
+    // object's $initLabels method, to ensure that the final fields are
+    // initialized first. Initializing the object fragments involves
+    // initializing their labels, which might depend on the final fields.
     for (String splitName : fabrw.getObjectFragments()) {
-      inits.add(
-          rw.qq().parseStmt("%s = (%s) new %s(this).fabric$lang$Object$();",
-              splitName, splitName, splitName));
+      inits.add(rw.qq().parseStmt("%s = new %s(this);", splitName, splitName));
     }
 
     // Add the remaining initializations.
@@ -255,10 +257,10 @@ public class ClassDeclToFabilExt_c extends ClassDeclToJavaExt_c {
       }
 
       return cb.addMember(rw.qq().parseMember(
-          "public Object %s() { " + "this.$updateLabel = %E;  "
-              + "this.$accessPolicy = %E.confPolicy();" + "return this;" + "}",
+          "public Object %s() { this.$updateLabel = %E; "
+              + "this.$accessPolicy = %E.confPolicy(); { %LS } return this; }",
           FabricToFabilRewriter.LABEL_INITIALIZER_METHOD_NAME, updateLabelExpr,
-          accessLabelExpr));
+          accessLabelExpr, inits));
     }
 
     return cb;

commit 7923509535d6218266298f38aec66580090c0302

When initializing object partitions, also initialize the super class's partitions. This fixes a regression introduced by commit 5901506909366a5614e82e93c41597d025c4bfb1.

src/compiler/fabric/translate/ClassDeclToFabilExt_c.java
@@ -25,6 +25,7 @@ import polyglot.ast.Formal;
 import polyglot.ast.Node;
 import polyglot.ast.Stmt;
 import polyglot.ast.TypeNode;
+import polyglot.qq.QQ;
 import polyglot.types.ClassType;
 import polyglot.types.Context;
 import polyglot.types.Flags;
@@ -52,7 +53,10 @@ public class ClassDeclToFabilExt_c extends ClassDeclToJavaExt_c {
   public Node toJavaImpl(JifToJavaRewriter rw) throws SemanticException {
     ClassDecl cd = (ClassDecl) super.toJavaImpl(rw);
 
-    cd = cd.body(addLabelInitializer(cd.body(), rw));
+    ClassBody body = cd.body();
+    body = addLabelInitializer(body, rw);
+    body = addPartitionInitializer(body, rw);
+    cd = cd.body(body);
 
     FabILNodeFactory nf = (FabILNodeFactory) rw.nodeFactory();
     FabILTypeSystem ts = (FabILTypeSystem) rw.java_ts();
@@ -249,20 +253,38 @@ public class ClassDeclToFabilExt_c extends ClassDeclToJavaExt_c {
 
       Expr accessLabelExpr = rw.labelToJava(accessLabel, simplify);
 
-      // Also initialize the object fragments.
-      FabricToFabilRewriter fabrw = (FabricToFabilRewriter) rw;
-      List<Stmt> inits = new ArrayList<>();
-      for (String splitName : fabrw.getObjectFragments()) {
-        inits.add(rw.qq().parseStmt("%s.fabric$lang$Object$();", splitName));
-      }
-
       return cb.addMember(rw.qq().parseMember(
           "public Object %s() { this.$updateLabel = %E; "
-              + "this.$accessPolicy = %E.confPolicy(); { %LS } return this; }",
+              + "this.$accessPolicy = %E.confPolicy(); %s(); return this; }",
           FabricToFabilRewriter.LABEL_INITIALIZER_METHOD_NAME, updateLabelExpr,
-          accessLabelExpr, inits));
+          accessLabelExpr,
+          FabricToFabilRewriter.PARTITION_INITIALIZER_METHOD_NAME));
     }
 
     return cb;
+  }
+
+  protected ClassBody addPartitionInitializer(ClassBody cb,
+      JifToJavaRewriter rw) {
+    FabricTypeSystem ts = (FabricTypeSystem) rw.jif_ts();
+    boolean sigMode = ((FabricToFabilRewriter) rw).inSignatureMode();
+
+    ClassDecl n = (ClassDecl) node();
+    FabricClassType ct = (FabricClassType) n.type();
+
+    if (sigMode || !ts.isFabricClass(ct)) return cb;
+
+    FabricToFabilRewriter fabrw = (FabricToFabilRewriter) rw;
+    QQ qq = fabrw.qq();
+    List<Stmt> stmts = new ArrayList<>();
+    stmts.add(qq.parseStmt("super.%s();",
+        FabricToFabilRewriter.PARTITION_INITIALIZER_METHOD_NAME));
+
+    for (String splitName : fabrw.getObjectFragments()) {
+      stmts.add(qq.parseStmt("%s.fabric$lang$Object$();", splitName));
+    }
+
+    return cb.addMember(rw.qq().parseMember("public void %s() { %LS }",
+        FabricToFabilRewriter.PARTITION_INITIALIZER_METHOD_NAME, stmts));
   }
 }

src/compiler/fabric/visit/FabricToFabilRewriter.java
@@ -40,6 +40,8 @@ import polyglot.util.Position;
 public class FabricToFabilRewriter extends JifToJavaRewriter {
   private static final Collection<String> TOPICS;
   public static final String LABEL_INITIALIZER_METHOD_NAME = "$initLabels";
+  public static final String PARTITION_INITIALIZER_METHOD_NAME =
+      "$initPartitions";
   static {
     TOPICS = new ArrayList<>(2);
     TOPICS.add("publish");

src/signatures/fabil/fabric/lang/Object.fil
@@ -17,6 +17,11 @@ public class Object extends java.lang.Object {
    */
   public native Object $initLabels();
   
+  /**
+   * Initializes the object's partitions.
+   */
+  public native void $initPartitions();
+  
   public native boolean equals(Object o);
   
   /**

src/system/fabric/lang/Object.java
@@ -74,6 +74,13 @@ public interface Object {
    */
   Object $initLabels();
 
+  /**
+   * Initializes the object's partitions (if any) by calling their initializer
+   * methods. Object partitions are created in jif$init(), but are initialized
+   * in this method, which should be called by $initLabels().
+   */
+  void $initPartitions();
+
   /** Whether this object is "equal" to another object. */
   boolean equals(Object o);
 
@@ -300,6 +307,11 @@ public interface Object {
     @Override
     public Object $initLabels() {
       return fetch().$initLabels();
+    }
+
+    @Override
+    public void $initPartitions() {
+      fetch().$initPartitions();
     }
 
     @Override
@@ -563,7 +575,18 @@ public interface Object {
       set$$accessPolicy(
           Worker.getWorker().getLocalStore().getBottomConfidPolicy());
 
+      $initPartitions();
+
       return $getProxy();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Subclasses overriding this method must call {@code super.$initPartitions()}.
+     */
+    @Override
+    public void $initPartitions() {
     }
 
     @Override
@@ -788,7 +811,7 @@ public interface Object {
         long accessPolicyOnum, ObjectInput serializedInput,
         Iterator<RefTypeEnum> refTypes, Iterator<Long> intraStoreRefs,
         Iterator<Pair<String, Long>> interStoreRefs)
-            throws IOException, ClassNotFoundException {
+        throws IOException, ClassNotFoundException {
       this(store, onum, version, expiry);
       this.$updateLabel = new Label._Proxy(updateLabelStore, updateLabelOnum);
       this.$accessPolicy =
@@ -828,7 +851,7 @@ public interface Object {
         Class<? extends Object._Proxy> proxyClass, RefTypeEnum refType,
         ObjectInput in, Store store, Iterator<Long> intraStoreRefs,
         Iterator<Pair<String, Long>> interStoreRefs)
-            throws IOException, ClassNotFoundException {
+        throws IOException, ClassNotFoundException {
       switch (refType) {
       case NULL:
         return null;
@@ -1055,7 +1078,7 @@ public interface Object {
           long accessPolicyOnum, ObjectInput serializedInput,
           Iterator<RefTypeEnum> refTypes, Iterator<Long> intraStoreRefs,
           Iterator<Pair<String, Long>> interStoreRefs)
-              throws IOException, ClassNotFoundException {
+          throws IOException, ClassNotFoundException {
         super(store, onum, version, expiry, updateLabelStore, updateLabelOnum,
             accessPolicyStore, accessPolicyOnum, serializedInput, refTypes,
             intraStoreRefs, interStoreRefs);

src/system/fabric/lang/WrappedJavaInlineable.java
@@ -102,6 +102,10 @@ public class WrappedJavaInlineable<T> implements JavaInlineable {
   }
 
   @Override
+  public void $initPartitions() {
+  }
+
+  @Override
   public Object fabric$lang$Object$() {
     return this;
   }