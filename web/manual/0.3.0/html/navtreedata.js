019b1973677e0dbc0b05083edf45239788cafbe6
Jed Liu <liujed@cs.cornell.edu>
2016-06-27 20:39:05 -0400

    Shipped Fabric 0.3.0.

---
+++ web/manual/0.3.0/html/navtreedata.js
@@ -0,0 +1,24 @@
+var NAVTREE =
+[
+  [ "Fabric", "index.html", [
+    [ "Introduction", "index.html", null ],
+    [ "Setting up Fabric", "setup.html", null ],
+    [ "The Fabric language", "language.html", null ],
+    [ "Compiling and publishing Fabric programs", "compiling.html", null ],
+    [ "The Fabric intermediate language", "fabil.html", null ],
+    [ "The Fabric runtime system", "runtime.html", "runtime" ],
+    [ "Running Fabric programs", "running.html", null ],
+    [ "Example programs", "examples.html", null ],
+    [ "Version history", "versions.html", null ],
+    [ "Credits", "credits.html", null ],
+    [ "Bibliography", "citelist.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"citelist.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ web/manual/0.3.0/html/navtreeindex0.js
@@ -0,0 +1,18 @@
+var NAVTREEINDEX0 =
+{
+"citelist.html":[10],
+"compiling.html":[3],
+"credits.html":[9],
+"examples.html":[7],
+"fabil.html":[4],
+"index.html":[0],
+"index.html":[],
+"language.html":[2],
+"node-config.html":[5,0],
+"pages.html":[],
+"running.html":[6],
+"runtime.html":[5],
+"setup.html":[1],
+"start-nodes.html":[5,1],
+"versions.html":[8]
+};
