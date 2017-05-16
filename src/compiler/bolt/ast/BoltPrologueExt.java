4abebe583c5bc81c176c2abf0bf0495ed1b0dcb4
Jed Liu <liujed@cs.cornell.edu>
2017-05-15 20:24:53 -0400

    Javadoc.

--- src/compiler/bolt/ast/BoltPrologueExt.java
+++ src/compiler/bolt/ast/BoltPrologueExt.java
@@ -2,6 +2,9 @@ package bolt.ast;
 
 import polyglot.util.SerialVersionUID;
 
+/**
+ * An extension node for constructor prologues.
+ */
 public class BoltPrologueExt extends BoltTermExt {
 
   private static final long serialVersionUID = SerialVersionUID.generate();
